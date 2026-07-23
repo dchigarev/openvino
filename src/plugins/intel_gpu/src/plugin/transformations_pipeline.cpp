e1129d8889bf39486d033f8ad5fe46600b89fddf
dchigarev <dmitry.chigarev@intel.com>
2026-07-23 13:20:23 +0000

    stage-10(fix non-gc-build)
    Signed-off-by: dchigarev <dmitry.chigarev@intel.com>

--- src/plugins/intel_gpu/src/plugin/transformations_pipeline.cpp
+++ src/plugins/intel_gpu/src/plugin/transformations_pipeline.cpp
@@ -153,7 +153,9 @@
 #include "transformations/init_node_info.hpp"
 #include "transformations/normalize_l2_decomposition.hpp"
 #include "transformations/low_precision/mark_dequantization_subgraph.hpp"
+#ifdef GRAPH_COMPILER
 #include "transformations/mlir/convert.hpp"
+#endif // GRAPH_COMPILER
 #include "transformations/op_conversions/bidirectional_sequences_decomposition.hpp"
 #include "transformations/op_conversions/convert_batch_to_space.hpp"
 #include "transformations/op_conversions/convert_broadcast3.hpp"
